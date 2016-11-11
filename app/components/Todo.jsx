62c278d47d3e038b60fe1351e27b5f0c5b56a3ca
Sangwook Cho <korean.crossfitter@gmail.com>
2016-11-11 19:37:36 +0900

    Style Todo App

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -8,6 +8,7 @@ class Todo extends Component {
 
   render() {
     var {id, text, completed, createdAt, completedAt} = this.props;
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
 
     var renderDate = () => {
       var message = 'Created'
@@ -17,17 +18,21 @@ class Todo extends Component {
         message = 'Completed';
         timestamp = completedAt;
       }
-      
+
       return `${message} ${moment.unix(timestamp).format('MMM Do YYYY @ h:mm a')}`;
     }
 
     return (
-      <div onClick={() => {
+      <div className={todoClassName} onClick={() => {
           this.props.onToggle(id)
         }}>
-        <input type="checkbox" checked={completed} />
-        <p>{text}</p>
-        <p>{renderDate()}</p>
+        <div>
+          <input type="checkbox" checked={completed} />
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
       </div>
     );
   }
--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -15,7 +15,7 @@ class TodoSearch extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container__header">
         <div>
           <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
         </div>
