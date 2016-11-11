996709b9e8c35c65f4c7d8cd45c54a345af6c05c
Sangwook Cho <korean.crossfitter@gmail.com>
2016-11-11 16:10:54 +0900

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,7 +18,39 @@ var getTodos = () => {
   return Array.isArray(todos) ? todos : [];
 };
 
+var filterTodos = (todos, showCompleted, searchText) => {
+  var filteredTodos = todos;
+
+  // filter by showCompleted
+  // if todo.completed === false, then it shows up
+  // if todo.completed === true, then it doesn't show up, but
+  // if showCompleted === true, then it shows up
+  filteredTodos = filteredTodos.filter(todo => {
+    return !todo.completed || showCompleted;
+  });
+
+  // filter by searchText
+
+  filteredTodos = filteredTodos.filter(todo => {
+    var todoText = todo.text.toLowerCase();
+    return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+  });
+
+  // sort todos with non-completed first
+  filteredTodos.sort((a, b) => {
+    if (a.completed === false && b.completed === true) {
+      return -1;
+    }
+    if (a.completed === true && b.completed === false) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return filteredTodos;
+}
+
 // Following also works
 // var TodoAPI = { setTodos, getTodos };
 
-export default { setTodos, getTodos };
+export default { setTodos, getTodos, filterTodos };
