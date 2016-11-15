ae0004d6403193ebdf4aa17a0d5fcba8a3371bb1
Sangwook Cho <korean.crossfitter@gmail.com>
2016-11-15 18:53:43 +0900

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,19 @@ import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 
 import TodoApp from 'TodoApp';
 
+import * as actions from 'actions';
+import {configure} from 'configureStore';
+
+var store = configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
